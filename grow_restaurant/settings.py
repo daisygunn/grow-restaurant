95ec0cc78d64400b5130bdd6dffba885253c736c
daisygunn <daisy.l.g@hotmail.co.uk>
2021-12-06 09:44:10 +0000

    Amend allowed_hosts variable

--- grow_restaurant/settings.py
+++ grow_restaurant/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -32,7 +31,7 @@ SECRET_KEY = 'DaisyGunnKeyPastelQuail'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["grow_restaurant.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["grow-restaurant.herokuapp.com", "localhost"]
 
 
 # Application definition
@@ -136,7 +135,7 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE ='cloudinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
