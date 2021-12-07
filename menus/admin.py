9777785e6337a95928a42ec91839f96ea6adfaf6
daisygunn <daisy.l.g@hotmail.co.uk>
2021-12-07 13:44:56 +0000

    Register food & drink models in menus.admin.py

--- menus/admin.py
+++ menus/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import FoodItems, DrinkItems
 
 # Register your models here.
+
+@admin.register(FoodItems)
+class FoodAdmin(admin.ModelAdmin):
+
+    list_filter = ('on_menu', 'updated_on')
+    list_display = ('dish_name', 'price', 'on_menu')
+
+
+@admin.register(DrinkItems)
+class FoodAdmin(admin.ModelAdmin):
+
+    list_filter = ('on_menu', 'updated_on')
+    list_display = ('drink_name', 'price', 'on_menu')
