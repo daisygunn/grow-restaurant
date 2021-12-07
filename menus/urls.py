f38917f516fe555b19612f06c47b6fbb95ddfd25
daisygunn <daisy.l.g@hotmail.co.uk>
2021-12-07 20:48:46 +0000

    Add drinks_menu view, url and template with initial content

--- menus/urls.py
+++ menus/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls import include, url
 urlpatterns = [
     path('', views.menus, name='menus/'),
     path('food_menu/', views.FoodMenu.as_view(), name='food_menu/'),
+    path('drinks_menu/', views.DrinksMenu.as_view(), name='drinks_menu/'),
 ]
--- menus/views.py
+++ menus/views.py
@@ -14,3 +14,8 @@ class FoodMenu(generic.ListView):
     model = FoodItems
     queryset = FoodItems.objects.filter(on_menu=True).order_by('food_menu_section')
     template_name = 'food_menu.html'
+
+class DrinksMenu(generic.ListView):
+    model = DrinkItems
+    queryset = DrinkItems.objects.filter(on_menu=True).order_by('drinks_menu_section')
+    template_name = 'drinks_menu.html'
