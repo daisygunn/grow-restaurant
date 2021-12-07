ec3c8651b93b8b8626c8bd75907f28d26f42ec7d
daisygunn <daisy.l.g@hotmail.co.uk>
2021-12-07 21:00:02 +0000

    Add null=True to drinkitems allergens

--- menus/models.py
+++ menus/models.py
@@ -27,7 +27,7 @@ class DrinkItems(models.Model):
     drink_name = models.CharField(max_length=200, unique=True)
     description = models.CharField(max_length=200, unique=True)
     price = models.IntegerField()
-    allergens = models.CharField(max_length=200)
+    allergens = models.CharField(max_length=200, null=True)
     drinks_menu_section = models.IntegerField(choices=DRINKS_MENU_SECTION, default=3)
     on_menu = models.BooleanField(default=False)
     updated_on = models.DateTimeField(auto_now=True)
