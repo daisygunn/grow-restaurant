303971cd335b13c76ca54012f4e676fcadd860a2
daisygunn <daisy.l.g@hotmail.co.uk>
2021-12-08 16:15:35 +0000

    Fix csrf issue with reservation form in post

--- reservations/views.py
+++ reservations/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
 from django.contrib import messages
+from django.template.context_processors import csrf
 from .models import Tables, Customer, Reservations
 from .forms import CustomerForm, ReservationForm
 from bootstrap_datepicker_plus import DateTimePickerInput
@@ -15,15 +16,17 @@ class ReservationsEnquiry(View):
         
         return render(
             request, 'reservations.html', 
-            {'customer_form': CustomerForm(), 'reservation_form': ReservationForm()},
+            {'customer_form': CustomerForm(), 'reservation_form': ReservationForm()}
             )
 
 
     def post(self, request, *args, **kwargs):
         customer_form = CustomerForm(data=request.POST)
         reservation_form = ReservationForm(request.POST)
+        # context = {}
+        # context.update(csrf(request))
 
         return render(
             request, 'reservations.html', 
-            {'customer_form': customer_form, 'reservation_form': reservation_form},
+            {'customer_form': customer_form, 'reservation_form': reservation_form}
             )
