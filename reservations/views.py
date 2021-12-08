3aa21035bd1c15745330f59152ea5fda8e80ffbe
daisygunn <daisy.l.g@hotmail.co.uk>
2021-12-08 12:13:41 +0000

    Add basic view form ReservationEnquiry & display forms on reservations.html

--- reservations/views.py
+++ reservations/views.py
@@ -10,10 +10,19 @@ from .forms import CustomerForm, ReservationForm
 class ReservationsEnquiry(View):
 
     def get(self, request, *args, **kwargs):
-        customer_form = CustomerForm(data=request.POST)
-        reservation_form = ReservationForm(request.POST)
+        
         template_name = "reservations.html"
         
+        return render(
+            request, 'reservations.html', 
+            {'customer_form': CustomerForm(), 'reservation_form': ReservationForm()},
+            )
+
+
+    def post(self, request, *args, **kwargs):
+        customer_form = CustomerForm(data=request.POST)
+        reservation_form = ReservationForm(request.POST)
+
         return render(
             request, 'reservations.html', 
             {'customer_form': customer_form, 'reservation_form': reservation_form},
