c5eb6cb6aabb84186b3d2511ab0c5e2105c7c895
daisygunn <daisy.l.g@hotmail.co.uk>
2021-12-08 08:45:41 +0000

    Update drinks_menu template & view to display each section of the menu seperately

--- menus/views.py
+++ menus/views.py
@@ -25,5 +25,13 @@ class FoodMenu(generic.ListView):
 
 class DrinksMenu(generic.ListView):
     model = DrinkItems
-    queryset = DrinkItems.objects.filter(on_menu=True).order_by('drinks_menu_section')
     template_name = 'drinks_menu.html'
+    context_object_name = 'drinks_items'
+
+    def get_queryset(self):
+        queryset = {
+            'hotdrinks_items': DrinkItems.objects.all().filter(on_menu=True, drinks_menu_section=0),
+            'fruitjuices_smoothies_items': DrinkItems.objects.all().filter(on_menu=True, drinks_menu_section=1),
+            'alcohol_items': DrinkItems.objects.all().filter(on_menu=True, drinks_menu_section=2)
+        }
+        return queryset
