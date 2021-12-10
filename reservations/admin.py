6d69206d026deb930f024da56ba2e468ab9a9631
daisygunn <daisy.l.g@hotmail.co.uk>
2021-12-10 07:29:36 +0000

    Add table_name field to Table model and remove min_no_people. Reduce number of guest_choices in reservation model and add table_id

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Customer, Tables, Reservations
 
 # Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+
+    list_display = ('full_name', 'email', 'phone_number')
+
+
+@admin.register(Tables)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('table_id', 'table_name', 'max_no_people')
+
+
+@admin.register(Reservations)
+class ReservationsAdmim(admin.ModelAdmin):
+    list_filter = ('no_of_guests', 'status', 'table_id')
+    list_display = ('reservation_id', 'customer_id', )
--- reservations/models.py
+++ reservations/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
+import phone_field.models
 import datetime
 
 # Create your models here.
@@ -13,23 +14,22 @@ class Customer(models.Model):
         return self.full_name
 
 
-class Tables(models.Model):
+class Table(models.Model):
     table_id = models.AutoField(primary_key=True)
-    min_no_people = models.IntegerField()
+    table_name = models.CharField(max_length=10, default="New table", unique=True)
     max_no_people = models.IntegerField()
 
     def __str__(self):
-        return self.table_id
+        return self.table_name
 
 
-class Reservations(models.Model):
+class Reservation(models.Model):
     reservation_id = models.AutoField(primary_key=True)
     customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="customer")
-    guests_choices = ((1, "1 person"), (2, "2 people"), (3, "3 people"),
-     (4, "4 people"), (5, "5 people"), (6, "6 people"), (7, "7 people"), 
-     (8, "8 people"), (9, "9 people"), (10, "10 people"))
+    guests_choices = ((1, "1 person"), (2, "2 people"), (3, "3 people"), (4, "4 people"))
     no_of_guests = models.IntegerField(choices=guests_choices, default=1)
     requested_time = models.DateTimeField()
+    table_id = models.ForeignKey(Tables, on_delete=models.CASCADE, related_name="table_booked", null=True)
     status_choices = ((0, "pending"), (0, "confirmed"))
     status = models.CharField(
         max_length=10, choices=status_choices, default=0)
