7547f87e7fd41c5ca4bd223d3ed9ec4ecf62fef8
daisygunn <daisy.l.g@hotmail.co.uk>
2021-12-31 11:27:26 +0000

    Create website/test_urls file with tests for index and contact_us urls

---
+++ website/tests_urls.py
@@ -0,0 +1,13 @@
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse, resolve
+from website.views import index, ContactPage
+
+# Create your tests here
+class TestUrls(SimpleTestCase):
+    def test_index_url_is_resolved(self):
+        url = reverse('index')
+        self.assertEquals(resolve(url).func, index)
+    
+    def test_contact_page_url_is_resolved(self):
+        url = reverse('contact_us')
+        self.assertEquals(resolve(url).func.view_class, ContactPage)
