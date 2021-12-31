47cf18db99469c47169a469da6b6e1daef942ca5
daisygunn <daisy.l.g@hotmail.co.uk>
2021-12-31 12:24:29 +0000

    Add test_views.py for website app and create tests for both index & contact_us

--- grow_restaurant/settings.py
+++ grow_restaurant/settings.py
@@ -123,17 +123,25 @@ WSGI_APPLICATION = 'grow_restaurant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+# if "DATABASE_URL" in os.environ:
+#     DATABASES = {
+#         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#     }
+# else:
+#     DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
---
+++ website/test_views.py
@@ -0,0 +1,33 @@
+from django.test import TestCase, Client
+from django.urls import reverse, resolve
+from django.core import mail
+
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.index_url = reverse('index')
+        self.contact_page_url = reverse('contact_us')
+
+    def test_index_GET(self):
+        response = self.client.get(self.index_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_contact_page_GET(self):
+        response = self.client.get(self.contact_page_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact_us.html')
+
+    def test_contact_page_POST_sends_email(self):
+        response = self.client.post(self.contact_page_url)
+
+        mail.send_mail('Subject here', 'Here is the message.',
+            'from@example.com', ['to@example.com'],
+            fail_silently=False)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, 'Subject here')
