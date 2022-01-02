6c97836d38f8db3efbd9e656d27f528f6e327cec
daisygunn <daisy.l.g@hotmail.co.uk>
2022-01-02 10:39:24 +0000

    Remove all previous reservations migration files to try and fix db error

--- grow_restaurant/settings.py
+++ grow_restaurant/settings.py
@@ -124,17 +124,18 @@ WSGI_APPLICATION = 'grow_restaurant.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
+if os.environ.get("DEVELOPMENT") == "True":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else: 
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL")),
+        }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-# if "DATABASE_URL" in os.environ:
-# DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-#     }
 # # else:
 # #     DATABASES = {
 # #     'default': {
@@ -143,17 +144,20 @@ WSGI_APPLICATION = 'grow_restaurant.wsgi.application'
 # #     }
 # # }
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+# if 'DATABASE_URL' in os.environ:
+#     DATABASES = {
+#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'TEST': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#         }
+#         }
+#     }
 
 
 # Password validation
