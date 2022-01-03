f33b735536a48378e5e262534b35091bf201b53a
daisygunn <daisy.l.g@hotmail.co.uk>
2022-01-03 12:20:57 +0000

    Add bootstrap columns for xl screens to index & medium/l on delete_reservation

--- grow_restaurant/settings.py
+++ grow_restaurant/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['grow-restaurant.herokuapp.com', 'localhost']
 
