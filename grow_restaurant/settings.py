f44caf5dbaa22161d48f0746e51ac10f5904cbf2
daisygunn <daisy.l.g@hotmail.co.uk>
2022-01-06 08:03:31 +0000

    Apply brand-text class to headings and instances of the word GROW. Change page-link, nav-link, social media icons hover colour to pink.

--- grow_restaurant/settings.py
+++ grow_restaurant/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['grow-restaurant.herokuapp.com', 'localhost']
 
@@ -188,7 +188,7 @@ USE_TZ = True
 
 DATE_INPUT_FORMAT = ['%d/%M/%Y', ]
 
-DATE_FORMAT = [ 'd m Y' ]
+DATE_FORMAT = 'd/m/Y'
 
 TIME_INPUT_FORMATS = ['%H:%M', ]
 
