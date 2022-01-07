d1e912e649a11cadda3368b70328d0e65dec7807
daisygunn <daisy.l.g@hotmail.co.uk>
2022-01-07 10:47:56 +0000

    Add missing dietary field from DrinkItems model. Change price field to float field to allow for more freedom for pricing.

--- menus/admin.py
+++ menus/admin.py
@@ -7,11 +7,11 @@ from .models import FoodItems, DrinkItems
 class FoodAdmin(admin.ModelAdmin):
 
     list_filter = ('on_menu', 'updated_on')
-    list_display = ('dish_name', 'price', 'food_menu_section', 'on_menu')
+    list_display = ('dish_name', 'food_menu_section', 'on_menu')
 
 
 @admin.register(DrinkItems)
 class DrinksAdmin(admin.ModelAdmin):
 
     list_filter = ('on_menu', 'updated_on')
-    list_display = ('drink_name', 'price', 'drinks_menu_section', 'on_menu')
+    list_display = ('drink_name', 'drinks_menu_section', 'on_menu')
