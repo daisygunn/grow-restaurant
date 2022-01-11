e82a58251fc5262f0f6c93b9398bfd26ffb5a140
daisygunn <daisy.l.g@hotmail.co.uk>
2022-01-11 12:46:38 +0000

    Write tests for reservations.views GET requests

---
+++ reservations/test_views.py
@@ -0,0 +1,64 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from reservations.models import Customer, Table, Reservation
+from django.contrib.auth.models import User
+
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='project.test@test.com', email='project.test@test.com', password='12345')
+        login = self.client.login(username='project.test@test.com', email='project.test@test.com', password='12345')   
+        self.reservations_url = reverse('reservations')
+        self.manage_reservations_url = reverse('manage_reservations')
+
+        table = Table.objects.create(
+            table_id = 5,
+            table_name = 'Table 5',
+            max_no_people = 4
+        )
+
+        customer = Customer.objects.create(
+            customer_id = 1,
+            full_name = 'Project Test',
+            email =  'project.test@test.com',
+            phone_number = '+447980987654'       
+        )
+
+        Reservation.objects.create(
+            reservation_id = 35,
+            customer_name = customer,
+            no_of_guests = 4,
+            requested_date = '2022-01-20',
+            requested_time = '12:00',
+            table_id = table,
+            status = 'pending'
+        )
+
+        self.edit_reservations_url = reverse('edit_reservation', args=[35])
+        self.delete_reservations_url = reverse('delete_reservation', args=[35])
+        self.edit_customer_details_url = reverse('edit_customer_details')
+
+    def test_reservation_GET(self):
+        response = self.client.get(self.reservations_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'reservations.html')
+
+    def test_manage_reservation_GET(self):
+        response = self.client.get(self.manage_reservations_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'manage_reservations.html')    
+
+    def test_delete_reservation_GET(self):
+        response = self.client.get(self.delete_reservations_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'delete_reservation.html')     
+
+    def test_edit_reservation_GET(self):
+        response = self.client.get(self.edit_reservations_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'edit_reservation.html')    
