76167ec4d4b2385c518a72f587ddbe875c617ffc
daisygunn <daisy.l.g@hotmail.co.uk>
2022-01-11 07:32:14 +0000

    Update JS file to hide map-section of footer on screens smaller than 769px. Put all JS inside functions that are called upon document being ready.

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,43 @@
+// Adds and removes classes in footer depending on screen width.
+function screenClass() {
+    if(window.innerWidth < 769) {
+        console.log("Screen is smaller")
+        $('#map-section').addClass('hidden')
+        $('#contact-section').addClass('md-6')
+    } else {
+        //pass
+    }
+}
+
 // JS for Forms
-
-$(document).ready(function () {
-    console.log('Working')
-
+function datePicker() {
     $("#id_requested_date").datepicker({ dateFormat: 'dd/mm/yy' });
+}
 
-    $("#delete-reservation").on('click', function() {
-        $('#confirmationModal').modal('show');
-    });
-
+function formError() {
     if( $(".alert.alert-danger").length )
     {
         $(".full-form").addClass("animate__animated animate__shakeX")
     }
+}
+
+// Opens the modal on delete_reservation
+function deleteModal() {
+    $("#delete-reservation").on('click', function() {
+        $('#confirmationModal').modal('show');
+    });
+}
+
+
+// Call all functions 
+$(document).ready(function () {
+
+    screenClass();
+
+    datePicker();
+    
+    deleteModal();
+
+    formError();
+
 });
