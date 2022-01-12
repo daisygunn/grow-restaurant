278b12fb84f15cdaa60a23aecd486243f0128da2
daisygunn <daisy.l.g@hotmail.co.uk>
2022-01-12 20:15:43 +0000

    Write tests for reservations.models

---
+++ reservations/test_models.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from reservations.models import Reservation, Table, Customer
+
+class TestModels(TestCase):
+
+    def setUp(self):
+        self.table = Table(table_id=1, table_name='Table 1', max_no_people=4)
+        self.customer = Customer(customer_id=12, full_name='Test 123', email='test123@gmail.com')
+        self.reservation = Reservation(
+            reservation_id=32, table_id=self.table, customer_name=self.customer, 
+            no_of_guests=4, requested_date='2022-01-23', 
+            requested_time='12:00', status='pending')
+
+    def test_create_table(self):
+        self.assertEqual(self.table.table_id, 1)
+        self.assertEquals(self.table.table_name, 'Table 1')
+        self.assertEquals(self.table.max_no_people, 4)
+
+
+
+    def test_create_customer(self):
+        self.assertEqual(self.customer.customer_id, 12)
+        self.assertEquals(self.customer.full_name, 'Test 123')
+        self.assertEquals(self.customer.email, 'test123@gmail.com')
+
+
+    def test_create_reservation(self):      
+        self.assertEqual(self.reservation.reservation_id, 32)
+        self.assertEquals(self.reservation.table_id, self.table)
+        self.assertEquals(self.reservation.customer_name, self.customer)
+        self.assertEquals(self.reservation.no_of_guests, 4)
+        self.assertEquals(self.reservation.requested_date, '2022-01-23')
+        self.assertEquals(self.reservation.requested_time, '12:00')
+        self.assertEquals(self.reservation.status, 'pending')
+        
