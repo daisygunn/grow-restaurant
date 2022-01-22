b5d87eee9a203b884363adcf1ab5c1f0ee31ba2c
daisygunn <daisy.l.g@hotmail.co.uk>
2022-01-22 17:10:24 +0000

    Update reservations.views tests

--- reservations/test_views.py
+++ reservations/test_views.py
@@ -29,18 +29,30 @@ class TestReservationsViews(TestCase):
             phone_number='+447980987654'
         )
 
-        self.reservation = Reservation.objects.create(
+        self.reservation1 = Reservation.objects.create(
             reservation_id=35,
             customer=self.customer,
             no_of_guests=4,
-            requested_date='2022-01-20',
+            requested_date='2022-05-20',
             requested_time='12:00',
             table=self.table,
             status='pending'
         )
 
-        self.edit_reservations_url = reverse('edit_reservation', args=[35])
-        self.delete_reservations_url = reverse('delete_reservation', args=[35])
+        self.reservation2 = Reservation.objects.create(
+            reservation_id=38,
+            customer=self.customer,
+            no_of_guests=2,
+            requested_date='2022-01-20',
+            requested_time='14:00',
+            table=self.table,
+            status='pending'
+        )
+
+        self.edit_reservation1_url = reverse('edit_reservation', args=[35])
+        self.delete_reservation1_url = reverse('delete_reservation', args=[35])
+        self.edit_reservation2_url = reverse('edit_reservation', args=[38])
+        self.delete_reservation2_url = reverse('delete_reservation', args=[38])
         self.edit_customer_details_url = reverse('edit_customer_details')
 
     def test_reservation_GET(self):
@@ -50,25 +62,62 @@ class TestReservationsViews(TestCase):
         self.assertTemplateUsed(response, 'reservations.html')
 
     def test_manage_reservation_GET(self):
-        if self.user.is_authenticated:
-            response = self.client.get(self.manage_reservations_url)
+        response = self.client.get(self.manage_reservations_url)
 
-            self.assertEquals(response.status_code, 200)
-            self.assertTemplateUsed(response, 'manage_reservations.html')
-        else:
-            self.assertEquals(response.status_code, 400)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'manage_reservations.html')
+
+    def test_manage_reservation_GET_unathorised_user_redirected(self):
+        self.client.logout()
+        response = self.client.get(self.manage_reservations_url)
+
+        self.assertEquals(response.status_code, 302)
 
     def test_delete_reservation_GET(self):
-        response = self.client.get(self.delete_reservations_url)
+        response = self.client.get(self.delete_reservation1_url)
 
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'delete_reservation.html')
 
+    def test_delete_reservation_GET_unathorised_user_redirected(self):
+        self.client.logout()
+        response = self.client.get(self.delete_reservation1_url)
+
+        self.assertEquals(response.status_code, 302)
+
     def test_edit_reservation_GET(self):
-        response = self.client.get(self.edit_reservations_url)
+        response = self.client.get(self.edit_reservation1_url)
 
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'edit_reservation.html')
+
+    def test_edit_reservation_GET_unathorised_user_redirected(self):
+        self.client.logout()
+        response = self.client.get(self.edit_reservation1_url)
+
+        self.assertEquals(response.status_code, 302)
+
+    def test_delete_reservation_GET_date_in_past_redirected(self):
+        response = self.client.get(self.delete_reservation2_url)
+
+        self.assertEquals(response.status_code, 302)
+
+    def test_edit_reservation_GET_date_in_past_redirected(self):
+        response = self.client.get(self.edit_reservation2_url)
+
+        self.assertEquals(response.status_code, 302)
+
+    def test_edit_customer_details_GET(self):
+        response = self.client.get(self.edit_customer_details_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'edit_customer_details.html')
+    
+    def test_edit_reservation_GET_unathorised_user_redirected(self):
+        self.client.logout()
+        response = self.client.get(self.edit_customer_details_url)
+
+        self.assertEquals(response.status_code, 302)
 
     def test_reservation_POST_adds_new_customer_and_reservation(self):
         table = self.table
@@ -80,8 +129,6 @@ class TestReservationsViews(TestCase):
             phone_number='+447980987654'
         )
 
-        customer.save()
-
         reservation = Reservation.objects.create(
             reservation_id=36,
             customer=customer,
@@ -92,22 +139,45 @@ class TestReservationsViews(TestCase):
             status='pending'
         )
 
-        reservation.save()
+        response = self.client.post(self.reservations_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(len(Reservation.objects.all()), 3)
+        self.assertEquals(len(Customer.objects.all()), 2)
+
+    def test_reservation_POST_does_not_add_customer_that_exists(self):
+        table = self.table
+        customer = self.customer 
+        reservation = Reservation.objects.create(
+            reservation_id=39,
+            customer=customer,
+            no_of_guests=4,
+            requested_date='2022-08-29',
+            requested_time='12:00',
+            table=table,
+            status='pending'
+        )
 
         response = self.client.post(self.reservations_url)
 
         self.assertEquals(response.status_code, 200)
-        self.assertEquals(len(Reservation.objects.all()), 2)
-        self.assertEquals(len(Customer.objects.all()), 2)
+        self.assertEquals(len(Reservation.objects.all()), 3)
+        self.assertEquals(len(Customer.objects.all()), 1)
 
     def test_edit_reservation_POST_updates_model(self):
-        # customer = self.customer
-        reservation = self.reservation
+        reservation = self.reservation1
+        
+        reservation.requested_date = '2022-04-01'
 
-        updated_date = '2022-04-01'
+        response = self.client.post(self.edit_reservation1_url)
 
-        reservation.requested_date = updated_date
+        self.assertEquals(self.reservation1.requested_date, '2022-04-01')
 
-        reservation.save()
+    def test_delete_reservation_POST_updates_model(self):
+        reservation = self.reservation2
+        response = self.client.post(self.delete_reservation2_url)
 
-        self.assertEquals(self.reservation.requested_date, '2022-04-01')
+        self.assertEquals(len(Reservation.objects.all()), 1)
+        self.assertNotIn(self.reservation2, Reservation.objects.all())
+
+
