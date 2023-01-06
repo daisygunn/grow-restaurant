8b2567113cc31b6c3e3bd6ac571d526282b41313
daisygunn <daisy.l.g@hotmail.co.uk>
2023-01-06 13:07:37 +0000

    Update homepage layout, remove animated header. Center text in hero image

--- grow_restaurant/settings.py
+++ grow_restaurant/settings.py
@@ -30,7 +30,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+if os.environ.get('DEVELOPMENT') == "True":
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['grow-restaurant.herokuapp.com', 'localhost']
 
